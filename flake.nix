ed501b0943fb3b422b3e6239203d73e9be9500d2
j-jeurissen <jeroenjeurissen@gmail.com>
2023-04-26 13:38:45 +0200

    changed user

--- flake.nix
+++ flake.nix
@@ -1,42 +1,34 @@
 {
   description = "my minimal flake";
-
-  # Input sources for required packages and configurations
   inputs = {
+    # Where we get most of our software. Giant mono repo with recipes
+    # called derivations that say how to build software.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # nixos-22.11
 
-    # Nixpkgs: A collection of derivations for building various software
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    # Home Manager: Manages user-specific configurations and symlinks
+    # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Darwin: Manages system-level software and settings, including fonts
+    # Controls system level software and settings including fonts
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Pwnvim: A customized Neovim configuration
+    # Tricked out nvim
     pwnvim.url = "github:zmre/pwnvim";
   };
-
-  # Output configurations for the system
   outputs = inputs@{ nixpkgs, home-manager, darwin, pwnvim, ... }: {
     darwinConfigurations.Jeroens-iMac = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       pkgs = import nixpkgs { system = "aarch64-darwin"; };
-
-      # List of modules to include in the configuration
       modules = [
         ./modules/darwin
         home-manager.darwinModules.home-manager
-
-        # Home Manager configuration
         {
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
             extraSpecialArgs = { inherit pwnvim; };
-            users.demo.imports = [ ./modules/home-manager ];
+            users.jeroen.imports = [ ./modules/home-manager ];
           };
         }
       ];
